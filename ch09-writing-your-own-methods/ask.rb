ceda317fd7c8894c3fbcc46d213ba06a0855c321
Tom Green <tmgree@outlook.com>
2015-11-11 17:58:16 +0000

    Update ask.rb

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,18 @@
-def ask question
-  # your code here
+def ask(question)
+puts question
+reply=gets.chomp.downcase
+
+while reply != 'yes' && reply != 'no'
+puts question
+reply=gets.chomp.downcase
+
+end
+if reply=='yes'
+return true
+end
+if reply=='no'
+    return false
+end
+
+
 end
