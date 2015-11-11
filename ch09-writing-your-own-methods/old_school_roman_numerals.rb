3b98f39f5075fb51217d65b0bebddbc3589d5cd3
Tom Green <tmgree@outlook.com>
2015-11-11 15:05:30 +0000

    Update old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,5 +1,5 @@
 def roman(n)
-   
+    
 somenumber=n%1000
 thousand=n-somenumber
 somenumber2=somenumber%100
@@ -9,40 +9,34 @@ tens=somenumber2-somenumber3
 units=somenumber3%10
 a=(thousand/1000)
 b=hundred/100
-if b>0 && b<4
+if b>0 && b<5
     roman1="M"*a+"C"*b
-elsif b==4
-roman1="M"*a+"CD"
+
 elsif b==5
 roman1="M"*a+"D"
-elsif b>5 && b<9
+elsif b>5 && b<10
 roman1="M"*a+"D"+"C"*(b%5)
-else
-    roman1="M"*a+"CM"
+
 end
 c=tens/10
-if c>0 &&c<4
+if c>0 &&c<5
     roman2=roman1+"X"*c
-    elsif c==4
-    roman2=roman1+"XL"
+    
     elsif c==5
     roman2=roman1+"L"
-    elsif c>5 && c<9
+    elsif c>5 && c<10
     roman2=roman1+"L"+"X"*(c%5)
-else
-    roman2=roman1+"XC"
+
 end
 d=n-thousand-hundred-tens
-if d>0 && d<4
+if d>0 && d<5
     roman3=roman2+"I"*d
-    elsif d==4
-    roman3=roman2+"IV"
+    
     elsif d==5
     roman3=roman2+"V"
-    elsif d>5 && d<9
+    elsif d>5 && d<10
     roman3=roman2+"V"+"I"*(d%5)
-else
-    roman3=roman2+"IX"
+
 end
 
 end
