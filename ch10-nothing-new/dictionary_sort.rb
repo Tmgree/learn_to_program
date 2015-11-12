7732406d2c41897475e2e94951548feeb5dc32c3
Tom Green <tmgree@outlook.com>
2015-11-12 20:47:41 +0000

    Update dictionary_sort.rb

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,19 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(arr)
+    list=arr.each do |x| 
+        x.capitalize!
+    end
+ return list if list.size <= 1
+  swapped = true
+  while swapped do
+    swapped = false
+    0.upto(list.size-2) do |i|
+      if list[i] > list[i+1]
+        list[i], list[i+1] = list[i+1], list[i] 
+        swapped = true
+      end
+    end    
+  end
+
+  list
+ 
 end
