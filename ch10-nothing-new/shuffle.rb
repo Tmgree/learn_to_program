504dcc0d6da07ed1ab10e6e3a27e1da5b0851396
Tom Green <tmgree@outlook.com>
2015-11-13 16:34:38 +0000

    Update shuffle.rb

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,18 @@
-def shuffle arr
-  # your code here
+def shufflez(arr)
+
+
+n=arr.length
+b=n
+z=Array.new(n)
+for i in (0 ... n) do
+
+k1=rand(n)
+z[i]=arr[k1]
+arr=arr-[arr[k1]]
+n=n-1
+end
+p z
+
+
+
 end
