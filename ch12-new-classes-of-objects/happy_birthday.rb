abc8663f35ffc21c27d5eb8af273ecd84699ef3f
Tom Green <tmgree@outlook.com>
2015-11-13 12:55:01 +0000

    Update happy_birthday.rb

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,25 @@
-# your code here
+def birth_Ask
+puts "What Year Were you Born in?"
+yeartemp=gets.chomp
+puts "What Month Were you Born in?"
+monthtemp=gets.chomp
+puts "What Day Were you Born on?"
+daytemp=gets.chomp
+
+year=yeartemp.to_i
+day=daytemp.to_i
+month=monthtemp.to_i
+
+a=Time.local(year, month, day)
+b=Time.new.year
+c=Time.new.month
+
+if c>=month
+d=b-year
+else
+d=b-year+1
+end
+puts "Spank"*d
+
+
+end
