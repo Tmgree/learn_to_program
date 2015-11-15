4ac4339b39fa69941e40cd7d786a5428ba1f803c
Tom Green <tmgree@outlook.com>
2015-11-15 12:52:33 +0000

    Update grandfather_clock.rb

--- ch14-blocks-and-procs/grandfather_clock.rb
+++ ch14-blocks-and-procs/grandfather_clock.rb
@@ -1,3 +1,10 @@
-def grandfather_clock &block
-  # your code here
+def grandfatherClock &block
+
+
+a=(Time.new.hour%12).to_i
+if a==0
+a=12
 end
+a.times(&block)
+end
+grandfatherClock{puts "Dong"}
